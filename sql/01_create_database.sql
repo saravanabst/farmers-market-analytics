13e7fc4f2a85a088d9ec5c9a8889fd7769c53ad4
Saravanakumar Kannan <saravanabst@gmail.com>
2026-07-08 10:49:41 +0530

    create farmers_market database

---
+++ sql/01_create_database.sql
@@ -0,0 +1,10 @@
+-- ==========================================
+-- Project : Farmers Market Analytics Platform
+-- File    : 01_create_database.sql
+-- Purpose : Create the project database
+-- Author  : Saravanakumur Kannan
+-- ==========================================
+
+CREATE DATABASE IF NOT EXISTS farmers_market;
+
+USE farmers_market;
